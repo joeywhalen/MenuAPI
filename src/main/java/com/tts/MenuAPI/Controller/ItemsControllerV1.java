8fbc06202e7f366039f45f3ecdff911238bb7127
Joey Whalen <jtktwhalen@gmail.com>
2021-03-26 14:05:26 -0400

    Updated ItemsControllerV1 and ItemV1

--- src/main/java/com/tts/MenuAPI/Controller/ItemsControllerV1.java
+++ src/main/java/com/tts/MenuAPI/Controller/ItemsControllerV1.java
@@ -1,4 +1,13 @@
 package com.tts.MenuAPI.Controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class ItemsControllerV1 {
+  @ApiOperation(value = "Get all menu items", response = Item.class,
+          responseContainer = "List")
+  @GetMapping("/items")
+  public ResponseEntity<List<Item>> getItems() {
+    return new ResponseEntity<>(new ArrayList<Item>(), HttpStatus.OK);
+  }
 }
--- src/main/java/com/tts/MenuAPI/Model/ItemV1.java
+++ src/main/java/com/tts/MenuAPI/Model/ItemV1.java
@@ -1,4 +1,19 @@
 package com.tts.MenuAPI.Model;
 
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
 public class ItemV1 {
+
+  private String name;
+  private String description;
+  private String category;
+  private List<String> ingredients;
+  private double price;
+  private Date createdDate;
+  private Date updatedDate;
+  
 }
