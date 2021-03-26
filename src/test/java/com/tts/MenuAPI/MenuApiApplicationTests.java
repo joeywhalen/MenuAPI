b1044e4f22b712ef4bc71712dddc5e555c584f52
Joey Whalen <jtktwhalen@gmail.com>
2021-03-26 12:42:30 -0400

    initial commit

---
+++ src/test/java/com/tts/MenuAPI/MenuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.MenuAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
